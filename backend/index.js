248d1a906dafb1b84fa85cd2ec6d841595d34370
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-01-16 23:25:10 +0530

    connect the server

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("💻 DB Synced");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`🚀💀 Server Started on Port ${process.env.PORT}`);
+});
